9185cfe18e390b72a80c99fcb18fc04456abfb91
Youssef Mhiri <youssef.mhiri@esprit.tn>
2021-02-26 14:49:56 +0100

    Sound Working

--- main.c
+++ main.c
@@ -3,9 +3,73 @@
 #include <stdlib.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_mixer.h>
-int main ()
+int main()
 {
+   //Declaration de variables;
 
-return 0 ; 
+   //Variables
+   int continuer = 1;
+   //Variables
+
+   //Images
+   SDL_Surface *screen = NULL;
+   //Images
+
+   //Events
+   SDL_Event event;
+   //Events
+
+   //Sound
+   Mix_Music *music;
+   Mix_Chunk *sound;
+   //Sound
+
+   //Declaration de variables;
+
+   //Initialisation video
+   SDL_Init(SDL_INIT_VIDEO);
+
+   if (SDL_Init(SDL_INIT_VIDEO) != 0)
+   {
+      printf("Unuable to initialize SDL: %s \n", SDL_GetError());
+      return 1;
+   }
+   //Initialisation video
+
+   screen = SDL_SetVideoMode(1260, 700, 32, SDL_HWSURFACE | SDL_RESIZABLE);
+
+   //Initialisation son
+   if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1)
+   {
+      printf("%s", Mix_GetError());
+   }
+   music = Mix_LoadMUS("Music.mp3");
+   Mix_PlayMusic(music,-1);
+   //Initialisation son
+
+   //Main Loop
+   while (continuer)
+   {
+      while (SDL_PollEvent(&event))
+      {
+         switch (event.type)
+         {
+         case (SDL_QUIT):
+            continuer = 0;
+            break;
+
+         case (SDL_KEYDOWN):
+         {
+            switch (event.key.keysym.sym)
+            {
+            case (SDLK_ESCAPE):
+               continuer = 0;
+               break;
+            }
+         }
+         }
+      }
+   }
+   //Main loop
+   return 0;
 }
-
